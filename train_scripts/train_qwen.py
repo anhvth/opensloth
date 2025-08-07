cbaea7189241179b359d21034fb5e8bd8e449d8a
anhvth <anhvth.226@gmail.com>
2025-08-07 17:09:17 +0000

    Remove unused dataset preparation scripts and update training configuration for Qwen3 model

--- train_scripts/train_qwen.py
+++ train_scripts/train_qwen.py
@@ -7,22 +7,23 @@ from opensloth.opensloth_config import (
 from opensloth.scripts.opensloth_sft_trainer import run_mp_training, setup_envs
 
 # 2 GPUs with packing configuration
-GLOBAL_BZ = 32
+GLOBAL_BZ = 64
 
-DEVICES = [0, 1]
+DEVICES = [0, 1,2,3]
 
 BZ = 1  # if sequence packing, then should be 1, larger does not contribute to speed
 opensloth_config = OpenSlothConfig(
-    data_cache_path="data/cache_qwen3_dataset/",
+    data_cache_path="data/cache_qwen3_dataset_250807/",
     devices=DEVICES,
     fast_model_args=FastModelArgs(
-        model_name="unsloth/Qwen3-0.6B-Base-bnb-4bit",
-        max_seq_length=16000,
+        model_name="./unsloth/Qwen3-32B-bnb-4bit",
+        # model_name='./unsloth/qwen3-0.6b-bnb-4bit',
+        max_seq_length=8000,
         load_in_4bit=True,
     ),
     lora_args=LoraArgs(
-        r=8,
-        lora_alpha=16,
+        r=256,
+        lora_alpha=512,
         target_modules=[
             "q_proj",
             "k_proj",
@@ -40,38 +41,24 @@ opensloth_config = OpenSlothConfig(
 )
 
 training_config = TrainingArguments(
-    output_dir="outputs/exps/qwen3-0.6b-FineTome-2gpu-packing",
-    resume_from_checkpoint="outputs/exps/qwen3-0.6b-FineTome-2gpu-packing",
-    save_only_model=False,
-    max_steps=10,
+    output_dir="outputs/exps/qwen3-30ba3-cache_qwen3_dataset_250807",
+    save_only_model=True,
     per_device_train_batch_size=BZ,
     gradient_accumulation_steps=GLOBAL_BZ // (len(DEVICES) * BZ),
     learning_rate=1e-5,
     logging_steps=1,
     num_train_epochs=1,
     lr_scheduler_type="linear",
-    warmup_steps=5,
+    warmup_steps=20,
     save_total_limit=1,
     weight_decay=0.01,
     optim="adamw_8bit",
     seed=3407,
-    report_to="none",  # or wandb/tensorboard
+    report_to="tensorboard",  # or wandb/tensorboard
 )
 
 
 if __name__ == "__main__":
     import os
-
-    # Setup wandb
-    os.environ["WANDB_PROJECT"] = "opensloth"
-    os.environ["WANDB_NAME"] = (
-        f"qwen3-0.6b_2gpu_packing_globalbz{GLOBAL_BZ}_samples10000"
-    )
-
-    print(
-        f"Global batch size: {len(DEVICES) * BZ * training_config.gradient_accumulation_steps}"
-    )
-    print(f"Gradient accumulation steps: {training_config.gradient_accumulation_steps}")
-
     setup_envs(opensloth_config, training_config)
     run_mp_training(opensloth_config.devices, opensloth_config, training_config)
